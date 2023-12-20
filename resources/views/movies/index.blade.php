4cd8a08b78fa3857850e23a5fa90af84bb15ba5a
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-12-20 16:52:15 +0100

    movie card

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,7 +3,28 @@
 @section('title', 'Movies')
 
 @section('content')
-    @foreach ($movies as $movie)
-        <li>{{ $movie->title }}</li>
-    @endforeach
+    <main>
+        <section class=" container ">
+            <h1>Movies</h1>
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="col-12 col-md-3 mt-5 mb-5">
+                        <div class="mycard">
+                            <div class="mycard-c">
+                                <img src="{{ $movie['image'] }}" class="card-img-top" alt="{{ $movie['title'] }}">
+                            </div>
+                            <div class=" mt-2 d-flex flex-column justify-content-between ">
+                                <div>
+                                    <h5>{{ $movie['title'] }}</h5>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+
+        </section>
+    </main>
+
 @endsection
